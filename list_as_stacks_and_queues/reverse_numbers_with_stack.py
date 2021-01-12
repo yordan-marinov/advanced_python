fde292fae67757638919ee41e226e7a93330ea56
yordan-marinov <jordanmarinov8@gmail.com>
2021-01-12 10:15:40 +0200

    Updated file: 'reverse_numberse'

--- list_as_stacks_and_queues/reverse_numbers_with_stack.py
+++ list_as_stacks_and_queues/reverse_numbers_with_stack.py
@@ -1,9 +1,19 @@
-numbers = input().split()
+# numbers = input().split()
+#
+# reversed_stack = []
+# for _ in range(len(numbers)):
+#     num = numbers.pop()
+#     reversed_stack.append(num)
+#
+#
+# print(" ".join(reversed_stack))
 
-reversed_stack = []
-for _ in range(len(numbers)):
-    num = numbers.pop()
-    reversed_stack.append(num)
+
+def reverse_numbers(numbers: list):
+    if not numbers:
+        return numbers
+
+    return reverse_numbers(numbers[1:]) + numbers[:1]
 
 
-print(" ".join(reversed_stack))
+print(*reverse_numbers(input().split()))
