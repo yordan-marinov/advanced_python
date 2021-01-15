2f0de6bbe080368e5102e9a895914f5d48bfa84a
yordan-marinov <jordanmarinov8@gmail.com>
2021-01-15 10:34:58 +0200

    Updated file: 'sets_of_elements'

--- tuples_and_sets/sets_of_elements.py
+++ tuples_and_sets/sets_of_elements.py
@@ -1,15 +1,25 @@
-def adding_to_set(number_length: int):
-    result = set(
-        [input() for _ in range(number_length)]
-    )
-    return result
+# def adding_to_set(number_length: int):
+#     result = set(
+#         [input() for _ in range(number_length)]
+#     )
+#     return result
+#
+#
+# def sets_of_elements():
+#     return adding_to_set(first_set_len).\
+#         intersection(adding_to_set(second_set_len))
+#
+#
+# first_set_len, second_set_len = tuple(map(int, input().split()))
+#
+# print("\n".join([str(x) for x in sets_of_elements()]))
+#
+# =================================================================
+
+def create_set(num: int) -> set:
+    return {input() for _ in range(num)}
 
 
-def sets_of_elements():
-    return adding_to_set(first_set_len).\
-        intersection(adding_to_set(second_set_len))
+n, m = [int(i) for i in input().split()]
 
-
-first_set_len, second_set_len = tuple(map(int, input().split()))
-
-print("\n".join([str(x) for x in sets_of_elements()]))
+print("\n".join(create_set(n).intersection(create_set(m))))
