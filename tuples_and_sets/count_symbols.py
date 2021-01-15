0658c3dae454f2c8df239a0e876ff1347c9ca175
yordan-marinov <jordanmarinov8@gmail.com>
2021-01-15 12:00:26 +0200

    Updated file: 'count_symbols'

--- tuples_and_sets/count_symbols.py
+++ tuples_and_sets/count_symbols.py
@@ -1,17 +1,42 @@
+# from collections import defaultdict
+#
+#
+# def count_symbols(text):
+#     symbols_count = defaultdict(int)
+#     for char in text:
+#         symbols_count[char] += 1
+#     return symbols_count
+#
+#
+# def print_statement(collection: dict):
+#     for key, value in sorted(collection.items()):
+#         print(f"{key}: {value} time/s")
+#
+#
+# t = input()
+# print_statement(count_symbols(t))
+#
+#===================================================
+
 from collections import defaultdict
 
 
-def count_symbols(text):
-    symbols_count = defaultdict(int)
-    for char in text:
-        symbols_count[char] += 1
-    return symbols_count
+def count_symbols(text: str) -> str:
+    def sort_symbols_alphabetical(dct: dict):
+        return sorted(dct.items(), key=lambda pair: pair[0])
+
+    def printing_line(elements):
+        return f"{elements[0]}: {elements[1]} time/s"
+
+    symbols = defaultdict(int)
+    for symbol in text:
+        symbols[symbol] += 1
+
+    return "\n".join(
+        printing_line(e)
+        for e in sort_symbols_alphabetical(symbols)
+    )
 
 
-def print_statement(collection: dict):
-    for key, value in sorted(collection.items()):
-        print(f"{key}: {value} time/s")
-
-
-t = input()
-print_statement(count_symbols(t))
+string = input()
+print(count_symbols(string))
