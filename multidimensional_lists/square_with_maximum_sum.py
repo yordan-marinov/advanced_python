07082b66ec5b3c0ac77a54fa4de7eadc98c2ce89
yordan-marinov <jordanmarinov8@gmail.com>
2021-01-17 22:23:41 +0200

    Updated file: 'square_with_maximum_sum'

--- multidimensional_lists/square_with_maximum_sum.py
+++ multidimensional_lists/square_with_maximum_sum.py
@@ -1,4 +1,7 @@
-def list_of_integers(separator=", "):
+from itertools import chain
+
+
+def matrix_from_input(separator=", "):
     rows, cols = [int(n) for n in input().split(separator)]
     mat_rix = [
         [int(n) for n in input().split(separator)]
@@ -7,41 +10,41 @@ def list_of_integers(separator=", "):
     return mat_rix
 
 
-def square_size(matrix):
-    squares = []
+def sum_of_matrix(matrix) -> int:
+    return sum(chain(*matrix))
+
+
+def make_matrix_from_matrix_by_size(matrix):
+    all_matrices = []
+
     for i in range(len(matrix) - 1):
-        r = matrix[i]
-        for j in range(len(r) - 1):
-            square = [
+        for j in range(len(matrix[i]) - 1):
+            new_matrix = [
                 [matrix[i][j], matrix[i][j + 1]],
                 [matrix[i + 1][j], matrix[i + 1][j + 1]],
             ]
-            squares.append(square)
-    return squares
+            all_matrices.append(new_matrix)
+
+    return all_matrices
 
 
-def max_square(matrix):
-    square_max_value = None
-    square_max = None
+def matrix_with_max_sum():
+    max_sum = None
+    result = None
+    for current_matrix in make_matrix_from_matrix_by_size(matrix_from_input()):
+        if max_sum is None or sum_of_matrix(current_matrix) > max_sum:
+            max_sum = sum_of_matrix(current_matrix)
+            result = current_matrix
 
-    for square in matrix:
-        square_sum = 0
-        for i in square:
-            square_sum += sum(i)
-
-        if square_max_value is None or square_sum > square_max_value:
-            square_max_value = square_sum
-            square_max = square
-
-    return square_max, square_max_value
+    return result
 
 
-biggest_square, maximum_sum = max_square(square_size(list_of_integers()))
+final_matrix = matrix_with_max_sum()
+print('\n'.join([' '.join(str(e) for e in row) for row in final_matrix]))
+print(sum_of_matrix(final_matrix))
 
-
-for row in biggest_square:
-    for column in row:
-        print(f"{column}", end=" ")
-    print()
-
-print(maximum_sum)
+# [
+#   [7, 1, 3, 3, 2, 1],
+#   [1, 3, 9, 8, 5, 6],
+#   [4, 6, 7, 9, 1, 0],
+# ]
