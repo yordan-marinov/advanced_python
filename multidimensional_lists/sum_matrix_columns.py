8ab11efea04daa019879bb7100337bd39121ee35
yordan-marinov <jordanmarinov8@gmail.com>
2021-01-17 14:08:33 +0200

    Updated file: 'sum_matrix_columns'

--- multidimensional_lists/sum_matrix_columns.py
+++ multidimensional_lists/sum_matrix_columns.py
@@ -1,49 +1,22 @@
-"""
-3, 3
-1 2 3
-4 5 6
-7 8 9
-
-"""
-
-
 def int_to_list_from_input(separator=" "):
     return [int(i) for i in input().split(separator)]
 
-#
-# def columns_sums(mtrx, row, col):
-#     sums = []
-#     for j in range(col):
-#         col_sum = 0
-#         for i in range(row):
-#             col_sum += mtrx[i][j]
-#         sums.append(col_sum)
-#     return sums
 
-
-rows_count, columns_count = int_to_list_from_input(", ")
+rows, columns = int_to_list_from_input(", ")
 
 matrix = [
     int_to_list_from_input()
-    for _ in range(rows_count)
+    for _ in range(rows)
 ]
-# for c in columns_sums(matrix, rows_count, columns_count):
-#     print(c)
 
+for col_index in range(cols):
+    col_sum = 0
+    for row in matrix:
+        col_sum += row[col_index]
 
+    print(col_sum)
 
-# matrix = []
-# for _ in range(rows_count):
-#     matrix.append(
-#         int_to_list_from_input()
-#     )
+# =======================================================
 
-
-# column_sum = [0] * columns_count
-#
-# for row in range(len(matrix)):
-#     for column in range(len(matrix[row])):
-#         column_sum[column] += matrix[row][column]
-#
 for columns in zip(*matrix):
     print(columns)
