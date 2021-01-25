3e69657923f21788dee284fbd96b700e007cfb15
yordan-marinov <jordanmarinov8@gmail.com>
2021-01-25 12:21:26 +0200

    Updated file: 'negative_vs_positive'

--- functions/negative_vs_positive.py
+++ functions/negative_vs_positive.py
@@ -1,19 +1,39 @@
-def negative_positive_pair(lst: list):
-    return [
-        [n for n in lst if n < 0],
-        [n for n in lst if n > 0],
-    ]
+def input_numbers() -> [int]:
+    return [int(n) for n in input().split()]
 
 
-numbers = [int(i) for i in input().split()]
+def positive_numbers_only(numbers) -> int:
+    def is_positive(num):
+        return num > 0
 
-negative = sum(negative_positive_pair(numbers)[0])
-positive = sum(negative_positive_pair(numbers)[1])
+    return sum([n for n in numbers if is_positive(n)])
 
-print(negative)
-print(positive)
 
-if abs(negative) > positive:
-    print("The negatives are stronger than the positives")
-else:
-    print("The positives are stronger than negatives")
+def negative_numbers_only(numbers) -> int:
+    def is_negative(num):
+        return num < 0
+
+    return sum([n for n in numbers if is_negative(n)])
+
+
+def compare_abs_of_negative_with_positive_numbers_sum(negative_sum, positive_sum, numbers) -> str:
+    if abs(negative_sum(numbers)) > positive_sum(numbers):
+        return f"The negatives are stronger than the positives"
+    return f"The positives are stronger than the negatives"
+
+
+def final_required_results(negative, positive, compared_result, numbers) -> print:
+    return f"""
+{negative(numbers)}
+{positive(numbers)}
+{compared_result(negative, positive, numbers)}
+"""
+
+
+print(
+    final_required_results(
+        negative_numbers_only,
+        positive_numbers_only,
+        compare_abs_of_negative_with_positive_numbers_sum,
+        input_numbers())
+)
