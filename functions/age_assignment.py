b4ed1c9e9433eb76757d2d5ff03b4aaceac2a8b6
yordan-marinov <jordanmarinov8@gmail.com>
2021-01-25 15:33:36 +0200

    Updated file: 'age_assignment'

--- functions/age_assignment.py
+++ functions/age_assignment.py
@@ -1,9 +1,26 @@
+# def age_assignment(*args, **kwargs):
+#     names_age = {}
+#     for name in args:
+#         if name[0] in kwargs.keys():
+#             names_age[name] = kwargs[name[0]]
+#     return names_age
+#
+#
+# print(age_assignment("Peter", "George", G=26, P=19))
+# print(age_assignment("Amy", "Bill", "Willy", W=36, A=22, B=61))
+
+
 def age_assignment(*args, **kwargs):
-    names_age = {}
-    for name in args:
-        if name[0] in kwargs.keys():
-            names_age[name] = kwargs[name[0]]
-    return names_age
+    def first_letter(word) -> str:
+        return word[0]
+
+    names = args
+    symbol_age = kwargs
+    return {
+        name: symbol_age[first_letter(name)]
+        for name in names
+        if first_letter(name) in symbol_age
+    }
 
 
 print(age_assignment("Peter", "George", G=26, P=19))
