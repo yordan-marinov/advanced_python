b4959ed17161c52e41ccdcbdfda3b785bdb90420
yordan-marinov <jordanmarinov8@gmail.com>
2021-01-25 10:48:55 +0200

    Updated file: 'sort'

--- functions/sort.py
+++ functions/sort.py
@@ -1,5 +1,20 @@
-def sort():
-    return sorted([int(i) for i in input().split()])
+# def sort():
+#     return sorted([int(i) for i in input().split()])
+#
+#
+# print(sort())
+
+def sort_numbers_increasing(func):
+    def inner(numbers):
+        return sorted(func(numbers))
+
+    return inner
 
 
-print(sort())
+@sort_numbers_increasing
+def input_numbers(numbers):
+    return numbers
+
+
+numbers = [int(n) for n in input().split()]
+print(input_numbers(numbers))
