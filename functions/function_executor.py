d6a517999249df843bae2fe4f17d2c32896a24b5
yordan-marinov <jordanmarinov8@gmail.com>
2021-01-25 15:16:34 +0200

    Updated file: 'function_executor'

--- functions/function_executor.py
+++ functions/function_executor.py
@@ -7,12 +7,7 @@ def sum_numbers(num1, num2):
 
 
 def func_executor(*args):
-    sum_pair = args[0][1]
-    multiply_pair = args[1][1]
-    return [
-        sum_numbers(*sum_pair),
-        multiply_numbers(*multiply_pair),
-    ]
+    return [func(*nums) for func, nums in args]
 
 
 print(func_executor((sum_numbers, (1, 2)), (multiply_numbers, (2, 4))))
