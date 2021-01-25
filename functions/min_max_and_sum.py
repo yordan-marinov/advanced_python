85a6b90a55d1fdee69358e1fb13742c9a3c30201
yordan-marinov <jordanmarinov8@gmail.com>
2021-01-25 11:34:30 +0200

    Updated file: 'min_max_and_sum'

--- functions/min_max_and_sum.py
+++ functions/min_max_and_sum.py
@@ -1,10 +1,19 @@
-def numbers():
-    return [int(i) for i in input().split()]
+# def numbers():
+#     return [int(i) for i in input().split()]
+#
+#
+# nums = numbers()
+#
+#
+# print(f"The minimum number is {min(nums)}")
+# print(f"The maximum number is {max(nums)}")
+# print(f"The sum number is: {sum(nums)}")
+def min_max_and_sum_of_numbers():
+    numbers = [int(n) for n in input().split()]
+    return f"""The minimum number is {min(numbers)}
+The maximum number is {max(numbers)}
+The sum number is: {sum(numbers)}
+"""
 
 
-nums = numbers()
-
-
-print(f"The minimum number is {min(nums)}")
-print(f"The maximum number is {max(nums)}")
-print(f"The sum number is: {sum(nums)}")
+print(min_max_and_sum_of_numbers())
