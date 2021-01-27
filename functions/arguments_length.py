e86784b3c07e9d1b368394118812fd1ea1395fb2
yordan-marinov <jordanmarinov8@gmail.com>
2021-01-27 13:20:25 +0200

    Updated file: 'arguments_lenght'-> Recursively solved

--- functions/arguments_length.py
+++ functions/arguments_length.py
@@ -5,3 +5,16 @@ def args_length(*args):
 print(args_length(1, 32, 5))
 print(args_length("john", "peter"))
 print(args_length([1, 2, 3]))
+
+
+# =========== Recursively solved ========================
+def args_length(*args):
+    if args == ():
+        return 0
+
+    return 1 + args_length(*args[1:])
+
+
+print(args_length(1, 32, 5))
+print(args_length("john", "peter"))
+print(args_length([1, 2, 3]))
