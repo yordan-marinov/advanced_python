ff6e409796d761e9d6012582ace81becb6e110e3
yordan-marinov <jordanmarinov8@gmail.com>
2021-01-27 17:36:08 +0200

    Updated file: 'keyword_arguments'-> Recursively solved

--- functions/keyword_arguments_length.py
+++ functions/keyword_arguments_length.py
@@ -5,3 +5,17 @@ def kwargs_length(**kwargs):
 dictionary = {'name': 'Peter', 'age': 25}
 
 print(kwargs_length(**dictionary))
+
+
+# =========== Recursively solved ==================
+def kwargs_length(**kwargs):
+    if kwargs == {}:
+        return 0
+
+    kwargs.popitem()
+    return 1 + kwargs_length(**kwargs)
+
+
+dictionary = {'name': 'Peter', 'age': 25}
+
+print(kwargs_length(**dictionary))
