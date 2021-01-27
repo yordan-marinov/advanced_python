252155c20624104870d2cb213a3a2f65de49c66f
yordan-marinov <jordanmarinov8@gmail.com>
2021-01-27 11:31:26 +0200

    Updated file: 'sort'-> Rcursively solved

--- functions/sort.py
+++ functions/sort.py
@@ -4,6 +4,7 @@
 #
 # print(sort())
 
+# As Decorator solved
 def sort_numbers_increasing(func):
     def inner(numbers):
         return sorted(func(numbers))
@@ -18,3 +19,18 @@ def input_numbers(numbers):
 
 numbers = [int(n) for n in input().split()]
 print(input_numbers(numbers))
+
+# Recursively solved
+numbers = [int(n) for n in input().split()]
+
+
+def sort(nums):
+    if not nums:
+        return []
+
+    min_num = min(nums)
+    nums.remove(min_num)
+    return [min_num] + sort(nums)
+
+
+print(sort(numbers))
