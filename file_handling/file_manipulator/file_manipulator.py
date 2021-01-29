5fd503eddecfef59af3889eba8553cdcab3fe2c1
yordan-marinov <jordanmarinov8@gmail.com>
2021-01-29 14:11:48 +0200

    Updated package: 'file_manipulator'

--- file_handling/file_manipulator/file_manipulator.py
+++ file_handling/file_manipulator/file_manipulator.py
@@ -1,5 +1,24 @@
 import os
 
+"""
+Create a program that will receive commands until the command "End". 
+The commands can be:
+    
+    • "Create-{file_name}" - Creates the given file with an empty content. 
+    If the file already exists, remove the existing text in it 
+    (as if the file is created again)
+    
+    • "Add-{file_name}-{content}" - Append the content and a new line after it. 
+    If the file does not exist, create it and add the content
+    
+    • "Replace-{file_name}-{old_string}-{new_string}" - Open the file and replace 
+    all the occurrences of the old string with the new string. 
+    If the file does not exist, print: "An error occurred"
+    
+    • "Delete-{file_name}" - Delete the file. 
+    If the file does not exist, print: "An error occurred"
+"""
+
 
 def create_file(file_name):
     with open(file_name, "w") as wf:
@@ -46,3 +65,19 @@ while True:
     data = data.split("-")
     command = data.pop(0)
     COMMANDS[command](*data)
+
+
+""""
+=========== This is example input ==================
+
+Create-file.txt
+Add-file.txt-First Line
+Add-file.txt-Second Line
+Replace-random.txt-Some-some
+Replace-file.txt-First-1st
+Replace-file.txt-Second-2nd
+Delete-random.txt
+Delete-file.txt
+End
+
+"""
