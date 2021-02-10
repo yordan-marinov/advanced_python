d043c4198f99597c9208c30c597017e2dfbdef1e
yordan-marinov <jordanmarinov8@gmail.com>
2021-02-10 09:05:34 +0200

    Added new file: 'constants' to 'email_validator package'

---
+++ error_handling/email_validator/constants.py
@@ -0,0 +1,3 @@
+MINIMUM_NAME_LENGTH = 4
+VALID_DOMAINS = ("com", "bg", "net", "org")
+AT_SYMBOL = "@"
--- error_handling/email_validator/email_validator.py
+++ error_handling/email_validator/email_validator.py
@@ -1,8 +1,5 @@
+from constants import MINIMUM_NAME_LENGTH, VALID_DOMAINS, AT_SYMBOL
 import exceptions
-
-MINIMUM_NAME_LENGTH = 4
-VALID_DOMAINS = ("com", "bg", "net", "org")
-AT_SYMBOL = "@"
 
 
 def validate_email(
@@ -11,6 +8,10 @@ def validate_email(
         domains=VALID_DOMAINS,
         at_symbol=AT_SYMBOL
 ):
+    def validate_at_symbol():
+        if at_symbol not in email:
+            raise exceptions.MustContainAtSymbolError
+
     def validate_length():
         name, mail = email.split("@")
         if len(name) <= length:
@@ -20,10 +21,6 @@ def validate_email(
         domain = email.split(".").pop()
         if domain not in domains:
             raise exceptions.InvalidDomainError
-
-    def validate_at_symbol():
-        if at_symbol not in email:
-            raise exceptions.MustContainAtSymbolError
 
     if not any([validate_at_symbol(), validate_length(), validate_domain()]):
         return "Email is valid"
