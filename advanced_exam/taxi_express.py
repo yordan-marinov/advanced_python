89f3351e48f62061a77c311a113b1d338ee8b5ae
yordan-marinov <jordanmarinov8@gmail.com>
2021-02-10 09:48:56 +0200

    Updated file: 'taxi_express'

--- advanced_exam/taxi_express.py
+++ advanced_exam/taxi_express.py
@@ -1,34 +1,43 @@
 from collections import deque
 
 
-def current_element(sequence_deque: deque) -> int:
-    return sequence_deque.popleft()
+def get_element(que: deque):
+    return que.popleft()
 
 
-def taxi_can_drive_customer(client, driver) -> bool:
-    return driver >= client
+def can_taxi_drive_customer(customer, taxi):
+    return taxi >= customer
 
 
-def leave_customer_at_the_que_beginning(que: deque, current_customer: int):
-    return que.appendleft(current_customer)
+def send_customer_back(customer, customers):
+    return customers.appendleft(customer)
 
 
-customers = deque(int(n) for n in input().split(", "))
-taxis = deque(int(n) for n in input().split(", ")[::-1])
-
-total_time = 0
-while customers and taxis:
-    customer = current_element(customers)
-    taxi = current_element(taxis)
-    if not taxi_can_drive_customer(customer, taxi):
-        leave_customer_at_the_que_beginning(customers, customer)
-        continue
-    total_time += customer
+def increase_total_time(customer_time, total_time):
+    total_time += customer_time
+    return total_time
 
 
-if not customers:
-    print(f"All customers were driven to their destinations")
-    print(f"Total time: {total_time} minutes")
-else:
-    print("Not all customers were driven to their destinations")
-    print(f"Customers left: {', '.join(str(c) for c in customers)}")
+def drive(customers, taxis):
+    total_time = 0
+    while True:
+        if not customers:
+            return f"All customers were driven to their destinations\n" \
+                   f"Total time: {total_time} minutes"
+        if not taxis:
+            return f"Not all customers were driven to their destinations\n" \
+                   f"Customers left: {', '.join(map(str, customers))}"
+
+        next_customer = get_element(customers)
+        next_taxi = get_element(taxis)
+
+        if can_taxi_drive_customer(next_customer, next_taxi):
+            total_time = increase_total_time(next_customer, total_time)
+        else:
+            send_customer_back(next_customer, customers)
+
+
+customers = deque([int(c) for c in input().split(", ")])
+taxis = deque([int(t) for t in input().split(", ")][::-1])
+
+print(drive(customers, taxis))
