458d4481affed28b8fa50f93fa772b045c6793d1
yordan-marinov <jordanmarinov8@gmail.com>
2021-02-12 20:33:14 +0200

    Updated file: 'miner'

--- multidimensional_lists/miner.py
+++ multidimensional_lists/miner.py
@@ -1,16 +1,22 @@
-def field_as_input_matrix(n) -> [[str]]:
-    return [input().split() for _ in range(n)]
+REGULAR_POSITION = "*"
+END_ROUTE = "e"
+COAL = "c"
+MINER_START_POSITION = 's'
 
 
-def starting_position(matrix, start_symbol) -> tuple:
+def get_field(size) -> [[str]]:
+    return [input().split() for _ in range(size)]
+
+
+def get_position(matrix, searched_symbol) -> tuple:
     side = len(matrix)
     for row in range(side):
         for col in range(side):
-            if matrix[row][col] == start_symbol:
+            if matrix[row][col] == searched_symbol:
                 return row, col
 
 
-def all_coals_on_field(matrix, coal_symbol) -> int:
+def all_coals_on_field(matrix, coal_symbol=COAL) -> int:
     total = 0
     for row in range(len(matrix)):
         total += matrix[row].count(coal_symbol)
@@ -18,62 +24,41 @@ def all_coals_on_field(matrix, coal_symbol) -> int:
     return total
 
 
-def next_move(current_pos, current_command) -> tuple:
-    moves = {"up": (-1, 0), "down": (1, 0), "left": (0, -1), "right": (0, 1)}
-    return tuple(sum(pair) for pair in zip(current_pos, moves[current_command]))
+def dig_coals(matrix, miner, commands_list, all_coals_count) -> str:
+    def next_move(current_pos, current_command) -> tuple:
+        moves = {"up": (-1, 0), "down": (1, 0), "left": (0, -1), "right": (0, 1)}
+        return tuple(sum(pair) for pair in zip(current_pos, moves[current_command]))
 
+    def valid_positions() -> bool:
+        return (
+                0 <= row_index < len(matrix) and
+                0 <= col_index < len(matrix)
+        )
 
-def valid_indexes(matrix, position) -> bool:
-    r, c = position
-    return 0 <= r < len(matrix) and 0 <= c < len(matrix)
+    coal_counter = 0
+    for command in commands_list:
+        row_index, col_index = next_move(miner, command)
+        if not valid_positions():
+            continue
 
+        if matrix[row_index][col_index] == COAL:
+            coal_counter += 1
+            if coal_counter == all_coals_count:
+                return f"You collected all coals! ({row_index}, {col_index})"
+            matrix[row_index][col_index] = REGULAR_POSITION
 
-def move_symbol(matrix, current_pos, next_pos, miner_symbol) -> [[str]]:
-    regular_position_symbol = "*"
-    matrix[current_pos[0]][current_pos[1]] = regular_position_symbol
-    matrix[next_pos[0]][next_position[1]] = miner_symbol
+        if matrix[row_index][col_index] == END_ROUTE:
+            return f"Game over! ({row_index}, {col_index})"
 
-    return matrix
+        miner = (row_index, col_index)
 
+    return f"{all_coals_count - coal_counter} coals left. {miner}"
 
-REGULAR_POSITION = "*"
-ROUTE_END = "e"
-COAL = "c"
-MINER_SYMBOL = "s"
 
 field_size = int(input())
-list_of_commands = input().split()
-field = field_as_input_matrix(field_size)
-miner_position = starting_position(field, MINER_SYMBOL)
-total_coals_on_field = all_coals_on_field(field, COAL)
+commands = input().split()
+field = get_field(field_size)
+miner_position = get_position(field, MINER_START_POSITION)
+all_coals = all_coals_on_field(field)
 
-coal_counter = 0
-game_over = False
-for command in list_of_commands:
-    current_position = miner_position
-    next_position = next_move(current_position, command)
-
-    if not valid_indexes(field, next_position):
-        continue
-
-    if field[next_position[0]][next_position[1]] == COAL:
-        coal_counter += 1
-        if coal_counter == total_coals_on_field:
-            print(f"You collected all coals! {next_position[0], next_position[1]}")
-            game_over = True
-            break
-        move_symbol(field, current_position, next_position, MINER_SYMBOL)
-
-    elif field[next_position[0]][next_position[1]] == ROUTE_END:
-        print(f"Game over! {next_position[0], next_position[1]}")
-        game_over = True
-        break
-
-    else:
-        move_symbol(field, current_position, next_position, MINER_SYMBOL)
-
-    miner_position = next_position
-
-# print("\n".join(" ".join(row) for row in field))
-if not game_over:
-    print(f"{total_coals_on_field - coal_counter} coals left. {miner_position[0], miner_position[1]}")
+print(dig_coals(field, miner_position, commands, all_coals))
