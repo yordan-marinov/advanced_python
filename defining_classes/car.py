193371f76dbe6c367e0f9285ed8db0659683bda9
yordan-marinov <jordanmarinov8@gmail.com>
2021-02-14 23:31:54 +0200

    "Fixed a typo"

--- defining_classes/car.py
+++ defining_classes/car.py
@@ -2,7 +2,7 @@ class Car:
     def __init__(self, name: str, model: str, engine: str):
         self.name = name
         self.model = model
-        self.engine = enginec
+        self.engine = engine
 
     def get_info(self):
         return f"This is {self.name} {self.model} " f"with engine {self.engine}"
