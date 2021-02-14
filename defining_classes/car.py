5fa4a6268c757b4a99af4e3d04434af9567c7750
yordan-marinov <jordanmarinov8@gmail.com>
2021-02-14 20:07:07 +0200

    Updated file: 'car'

--- defining_classes/car.py
+++ defining_classes/car.py
@@ -1,13 +1,12 @@
 class Car:
-
     def __init__(self, name: str, model: str, engine: str):
         self.name = name
         self.model = model
-        self.engine = engine
+        self.engine = enginec
 
     def get_info(self):
-        return f"This is {self.name} {self.model} " \
-               f"with engine {self.engine}"
+        return f"This is {self.name} {self.model} " f"with engine {self.engine}"
+
 
 car = Car("Kia", "Rio", "1.3L B3 I4")
 print(car.get_info())
