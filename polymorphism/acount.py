128b6aa8d133c6aabe429e047df9783934645bd4
yordan-marinov <jordanmarinov8@gmail.com>
2021-02-27 12:22:17 +0200

    Added new file: 'account'

---
+++ polymorphism/acount.py
@@ -0,0 +1,76 @@
+class Account:
+    def __init__(self, owner: str, amount=0):
+        self.owner: str = owner
+        self.amount: int = amount
+        self._transactions = []
+
+    def add_transaction(self, amount):
+        if not isinstance(amount, int):
+            raise ValueError("please use int for amount")
+        self._transactions.append(amount)
+
+    @property
+    def balance(self):
+        return sum(self._transactions) + self.amount
+
+    def validate_transaction(self, account, amount_to_add: int):
+        if account.balance + amount_to_add < 0:
+            raise ValueError("sorry cannot go in debt!")
+        account.add_transaction(amount_to_add)
+        return f"New balance: {account.balance}"
+
+    def __str__(self):
+        return f"Account of {self.owner} with starting amount: {self.amount}"
+
+    def __repr__(self):
+        return f"Account({self.owner}, {self.amount})"
+
+    def __len__(self):
+        return len(self._transactions)
+
+    def __getitem__(self, index):
+        return self._transactions[index]
+
+    def __eq__(self, other):
+        return self.balance == other.balance
+
+    def __lt__(self, other):
+        return self.balance < other.balance
+
+    def __ge__(self, other):
+        return self.balance >= other.balance
+    
+    def __add__(self, other):
+        owner = f"{self.owner}&{other.owner}"
+        amount = self.amount
+        shared_account = Account(owner, amount)
+        shared_account._transactions = self._transactions + other._transactions
+        
+        return shared_account
+
+
+acc = Account("bob", 10)
+acc2 = Account("john")
+print(acc)
+print(repr(acc))
+acc.add_transaction(20)
+acc.add_transaction(-20)
+acc.add_transaction(30)
+print(acc.balance)
+print(len(acc))
+for transaction in acc:
+    print(transaction)
+print(acc[1])
+print(list(reversed(acc)))
+acc2.add_transaction(10)
+acc2.add_transaction(60)
+print(list(acc2))
+print(acc > acc2)
+print(acc >= acc2)
+print(acc < acc2)
+print(acc <= acc2)
+print(acc == acc2)
+print(acc != acc2)
+acc3 = acc + acc2
+print(acc3)
+print(acc3._transactions)
