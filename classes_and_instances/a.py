43679e858ad7171a71bb1f4571233772c8f60c80
yordan-marinov <jordanmarinov8@gmail.com>
2021-03-24 11:21:25 +0200

    Updated file: 'cup'

--- classes_and_instances/a.py
+++ classes_and_instances/a.py
@@ -7,17 +7,14 @@ class Cup:
     def free_space(self):
         return self.size - self.quantity
     
-    def enough_free_space_to_fill(self, value):
-        return value <= self.free_space
-    
     def fill(self, milliliters):
-        if self.enough_free_space_to_fill(milliliters):
+        if  milliliters <= self.free_space:
             self.quantity += milliliters
     
     def status(self):
         return self.free_space
-
-cup = Cup(100, 50)
+    
+cup= Cup(100, 50)
 cup.fill(50)
 cup.fill(10)
 print(cup.status())
